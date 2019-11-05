de285e883d4d4bd1231d4ee18f85408f70879fc4
636302 <636302@0338S-Z-90TDE0T.sdcs.local>
2019-11-05 14:20:26 -0800

    Robot square

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -11,23 +11,22 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
-    	// 1. Make a new Robot
-
+    	// 1. Make a new R
+    	Robot Hogan = new Robot();
 
         // 3. Put the robot's pen down
-
-
+        Hogan.penDown();
+            
         // 6. Make the robot move as fast as possible
-
-
+        Hogan.setSpeed(10);
         // 5. Do everything below here 4 times
-
-
+        for (int i = 0; i < 4; i++) {
+        	
         //         2. Move your robot 200 pixels
-
-
+                      Hogan.move(200);
+                    
         //         4. Turn the robot 90 degrees to the right (90 degrees)
+                   Hogan.turn(90);
 
-
-    }
+    }}
 }
